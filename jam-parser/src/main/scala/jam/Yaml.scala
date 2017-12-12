e583b75988f00039435c53c9cb9277d5b3c48174
Leandro Bolivar <leandrob131@gmail.com>
2017-12-11 23:50:11 -0500

    Added first parsers for Yaml objects

--- jam-parser/src/main/scala/jam/Yaml.scala
+++ jam-parser/src/main/scala/jam/Yaml.scala
@@ -7,13 +7,22 @@ sealed abstract class Yaml extends Product with Serializable
 object Yaml {
 
   private[jam] final case object YNull extends Yaml
+
   private[jam] final case object YTrue extends Yaml
+
   private[jam] final case object YFalse extends Yaml
+
   private[jam] final case class YInt(v: Int) extends Yaml
+
   private[jam] final case class YLong(v: Long) extends Yaml
+
   private[jam] final case class YFloat(v: Float) extends Yaml
+
   private[jam] final case class YDouble(v: Double) extends Yaml
+
   private[jam] final case class YString(v: String) extends Yaml
+
   private[jam] final case class YArray(v: Vector[Yaml]) extends Yaml
+
   private[jam] final case class YMap(v: ListMap[String, Yaml]) extends Yaml
 }
--- jam-parser/src/main/scala/jam/parser/YamlParser.scala
+++ jam-parser/src/main/scala/jam/parser/YamlParser.scala
@@ -1,5 +1,20 @@
 package jam.parser
 
 import fastparse.all._
+import jam.Yaml
 
-class YamlParser {}
+class YamlParser {
+
+  val strings = P(AnyChar.rep)
+
+  val digits = P(CharPred(_.isDigit).rep)
+
+  val ints = digits.!.map(x => Yaml.YInt(x.toInt))
+
+  val longs = digits.!.map(x => Yaml.YLong(x.toLong))
+
+  val floats = P("." ~ digits).!.map(x => Yaml.YFloat(x.toFloat))
+
+  val doubles = P("." ~ digits).!.map(x => Yaml.YDouble(x.toDouble))
+
+}
