17c07bc3dec5d2b69923efe6262da5c699ad1db6
Leandro Bolivar <leandrob131@gmail.com>
2017-12-13 01:51:34 -0500

    Added tests, fixed generators

--- jam-parser/src/test/scala/jam/parser/Generators.scala
+++ jam-parser/src/test/scala/jam/parser/Generators.scala
@@ -6,21 +6,29 @@ import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
 trait Generators extends GeneratorDrivenPropertyChecks {
 
-  val stringGen =
-    listOf(Gen.frequency((5, Gen.alphaNumStr), (3, " "), (2, Arbitrary.arbString.arbitrary))).map(_.mkString)
-
   val arbitraryString = Arbitrary.arbString
 
   val arbitraryDouble = Arbitrary.arbDouble
 
-  //val digitGen = listOf(Gen.numChar.filter(_.isDigit)).map(_.mkString).suchThat(_.nonEmpty)
+  val arbitraryFloat = Arbitrary.arbFloat
+
+  val stringGen =
+    listOf(Gen.frequency((5, Gen.alphaNumStr), (3, " "))).map(_.mkString)
+
   val negativeIntGen = Gen.chooseNum[Int](-2147483648, -1)
 
   val positiveIntGen = Gen.chooseNum[Int](0, 2147483647)
 
+  val negativeLongGen = Gen.choose[Long](-9223372036854775808L, -1L)
+
+  val positiveLongGen: Gen[Long] = Gen.chooseNum[Long](0L, 9223372036854775807L)
+
+  val positiveFloatGen = arbitraryFloat.arbitrary.filter(_ >= 0D)
+
+  val negativeFloatGen = arbitraryFloat.arbitrary.filter(_ < 0D)
+
   val positiveDoubleGen = arbitraryDouble.arbitrary.filter(_ >= 0D)
 
   val negativeDoubleGen = arbitraryDouble.arbitrary.filter(_ < 0D)
-  //.map(_.toString)
-  //.suchThat(x => x.nonEmpty && x.forall(_.isDigit)) //Gen.numStr.suchThat(x => x.nonEmpty && x.forall(_.isDigit)) //arbitraryString.arbitrary.suchThat(_.forall(_.isDigit))
+
 }
--- jam-parser/src/test/scala/jam/parser/YamlParserTest.scala
+++ jam-parser/src/test/scala/jam/parser/YamlParserTest.scala
@@ -2,7 +2,11 @@ package jam.parser
 
 import fastparse.core.Parsed
 import jam.Yaml
+import jam.Yaml.{ YInt, YMap, YString, YTrue }
 import org.scalatest.{ Inside, MustMatchers, WordSpec }
+
+import scala.collection.immutable.ListMap
+import scala.io.Source
 
 class YamlParserTest extends WordSpec with MustMatchers with Inside with Generators {
 
@@ -39,9 +43,44 @@ class YamlParserTest extends WordSpec with MustMatchers with Inside with Generat
       }
     }
 
+    "parse a positive Long" in {
+      forAll(positiveLongGen) { s: Long =>
+        inside(parser.longs.parse(s.toString)) {
+          case Parsed.Success(v, _) =>
+            v mustBe Yaml.YLong(s)
+        }
+      }
+    }
+
+    "parse a negative Long" in {
+      forAll(negativeLongGen) { s: Long =>
+        inside(parser.longs.parse(s.toString)) {
+          case Parsed.Success(v, _) =>
+            v mustBe Yaml.YLong(s)
+        }
+      }
+    }
+
+    "parse a positive Float" in {
+      forAll(positiveFloatGen) { s: Float =>
+        inside(parser.floats.parse(s.toString)) {
+          case Parsed.Success(v, _) =>
+            v mustBe Yaml.YFloat(s)
+        }
+      }
+    }
+
+    "parse a negative Float" in {
+      forAll(negativeFloatGen) { s: Float =>
+        inside(parser.floats.parse(s.toString)) {
+          case Parsed.Success(v, _) =>
+            v mustBe Yaml.YFloat(s)
+        }
+      }
+    }
+
     "parse a positive Double" in {
       forAll(positiveDoubleGen) { s: Double =>
-        println(s"============$s")
         inside(parser.doubles.parse(s.toString)) {
           case Parsed.Success(v, _) =>
             v mustBe Yaml.YDouble(s)
@@ -51,15 +90,56 @@ class YamlParserTest extends WordSpec with MustMatchers with Inside with Generat
 
     "parse a negative Double" in {
       forAll(negativeDoubleGen) { s: Double =>
-        println(s"============$s")
         inside(parser.doubles.parse(s.toString)) {
           case Parsed.Success(v, _) =>
-            println(s"=================V $v")
-            println(s"=================XXXXX ${parser.doubles.parse(s.toString)}")
             v mustBe Yaml.YDouble(s)
         }
       }
     }
+
+    "parse a true" in {
+      inside(parser.True.parse("true")) {
+        case Parsed.Success(v, _) =>
+          v mustBe Yaml.YTrue
+      }
+
+      inside(parser.True.parse("True")) {
+        case Parsed.Success(v, _) =>
+          v mustBe Yaml.YTrue
+      }
+    }
+
+    "parse a false" in {
+      inside(parser.False.parse("false")) {
+        case Parsed.Success(v, _) =>
+          v mustBe Yaml.YFalse
+      }
+
+      inside(parser.False.parse("False")) {
+        case Parsed.Success(v, _) =>
+          v mustBe Yaml.YFalse
+      }
+    }
+
+    "parse an object" in {
+      val value = getYaml("/simple.yaml")
+
+      inside(parser.obj.parse(value)) {
+        case Parsed.Success(v, 35) =>
+          v mustBe YMap(
+            ListMap(
+              "name"    -> YString("Ralph"),
+              "age"     -> YInt(33),
+              "married" -> YTrue
+            )
+          )
+      }
+    }
   }
+
+  def getYaml(path: String) =
+    Source
+      .fromInputStream(this.getClass.getResourceAsStream(path))
+      .mkString
 
 }
