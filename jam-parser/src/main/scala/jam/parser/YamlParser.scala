0131949a94936fd03f685f2d63293e744513c7fa
Leandro Bolivar <leandrob131@gmail.com>
2017-12-13 01:50:47 -0500

    Fixed errors in parser

--- jam-parser/src/main/scala/jam/parser/YamlParser.scala
+++ jam-parser/src/main/scala/jam/parser/YamlParser.scala
@@ -1,15 +1,33 @@
 package jam.parser
 
+import fastparse.all
 import fastparse.all._
 import jam.Yaml
+import jam.Yaml.YMap
+
+import scala.collection.immutable.ListMap
 
 class YamlParser {
 
-  val digits = P(CharIn("+-").? ~ CharPred(_.isDigit).rep)
+  val stringChars: Char => Boolean = !":\n\"\\".contains(_: Char)
+
+  val hexDigit = P(CharIn('0' to '9', 'a' to 'f', 'A' to 'F'))
+
+  val unicodeEscape = P("u" ~ hexDigit ~ hexDigit ~ hexDigit ~ hexDigit)
+
+  val escape = P("\\" ~ (CharIn("\"/\\bfnrt") | unicodeEscape))
+
+  val strChars = P(CharsWhile(stringChars))
+
+  val space = P(CharsWhileIn(" \r\n").?)
+
+  val digits = P(CharIn("+-").? ~ CharsWhileIn("0123456789"))
 
   val decimals = P(digits ~ "." ~ digits ~ (CharIn("eE") ~ digits).?)
 
-  val strings = P(AnyChar.rep).!.map(x => Yaml.YString(x))
+  val keys = P((strChars | escape).rep).!
+
+  val strings = P("\"".? ~/ (strChars | escape).rep.! ~ "\"".?).map(x => Yaml.YString(x))
 
   val ints = digits.!.map(x => Yaml.YInt(x.toInt))
 
@@ -22,5 +40,12 @@ class YamlParser {
   val True = P("True" | "true").!.map(_ => Yaml.YTrue)
 
   val False = P("False" | "false").!.map(_ => Yaml.YFalse)
+
+  val pair = P(keys ~/ ":" ~/ expr)
+
+  val obj = P(pair.rep(sep = "\n".~/)).map(x => YMap(ListMap(x: _*)))
+
+  val expr: all.Parser[Yaml] =
+    P(" ".? ~ (True | False | ints | strings) ~ &("\n").?)
 
 }
